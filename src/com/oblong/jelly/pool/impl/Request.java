86442024c0e7ff8234d2aa577f018be2417eec2d
jjj <jjj@oooooo.com>
2010-06-28 17:41:39 +0200

    TCP v2 vs v3 taken into account.

--- src/com/oblong/jelly/pool/impl/Request.java
+++ src/com/oblong/jelly/pool/impl/Request.java
@@ -16,92 +16,98 @@ import com.oblong.jelly.pool.ProtocolException;
  */
 public enum Request {
     CREATE(0, 3, 1) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(res, 0);
         }
     },
     DISPOSE(1, 1, 1) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(res, 0);
         }
     },
     PARTICIPATE(2, 2, 1) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(res, 0);
         }
     },
     PARTICIPATE_C(3, 4, 1){
-        Slaw getRetort(Slaw res) throws ProtocolException {
-            return retort(res, 0);
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
+            final Slaw ret = retort(res, 0);
+            if (v < 3) {
+                final long c = ret.emitLong();
+                if (c == ServerErrorCode.POOL_EXISTS_V3.code())
+                    return Slaw.int64(0);
+            }
+            return ret;
         }
     },
     WITHDRAW(4, 0, 1) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(res, 0);
         }
     },
     DEPOSIT(5, 1, 3) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return index(retort(index(res, 0), 1), 2);
         }
     },
     NTH_PROTEIN(6, 1, 3){
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(stamp(protein(res, 0), 1), 2);
         }
     },
     NEXT(7, 1, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 0), 1), 2), 3);
         }
     },
     PROBE_FWD(8, 2, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 0), 1), 2), 3);
         }
     },
     NEWEST_INDEX(9, 0, 2) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(res, 0), 1);
         }
     },
     OLDEST_INDEX(10, 0, 2) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(res, 0), 1);
         }
     },
     AWAIT_NEXT(11, 1, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 1), 2), 3), 0);
         }
     },
     ADD_AWAITER(12, 0, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 1), 2), 3), 0);
         }
     },
     INFO(15, 1, 2) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return protein(retort(res, 0), 1);
         }
     },
     LIST(16, 0, 2) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(res, 0);
         }
     },
     INDEX_LOOKUP(17, 3, 2) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(res, 0), 1);
         }
     },
     PROBE_BACK(18, 2, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 0), 1), 2), 3);
         }
     },
     PREV(19, 1, 4) {
-        Slaw getRetort(Slaw res) throws ProtocolException {
+        Slaw getRetort(Slaw res, int v) throws ProtocolException {
             return retort(index(stamp(protein(res, 0), 1), 2), 3);
         }
     };
@@ -128,16 +134,16 @@ public enum Request {
         }
     }
 
-    abstract Slaw getRetort(Slaw lst) throws ProtocolException;
+    abstract Slaw getRetort(Slaw res, int v) throws ProtocolException;
 
     private Slaw checkResponse(Slaw res, int v) throws PoolException {
         if (res.count() < responseArity)
             throw new ProtocolException(res, "Wrong response arity "
                                             + res.count() + " ("
                                             + responseArity + " expected)");
-        final Slaw ret = getRetort(res);
+        final Slaw ret = getRetort(res, v);
         final ServerError err = ServerError.getError(v, ret);
-        if (err != ServerError.SPLEND) 
+        if (err != ServerError.SPLEND)
             throw err.asException(res, ret.emitLong());
         return res;
     }
