54ace5f6f466e5cf87503d4081cd49dee8fe75fb
jjj <jjj@oooooo.com>
2010-07-29 21:28:59 +0200

    Docstrings for PoolAddress and PoolServerAddress.

--- src/com/oblong/jelly/PoolAddress.java
+++ src/com/oblong/jelly/PoolAddress.java
@@ -1,29 +1,72 @@
 // Copyright (c) 2010 Oblong Industries
+// Created: Thu Jul  1 15:56:36 2010
 
 package com.oblong.jelly;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import net.jcip.annotations.Immutable;
 
 /**
+ * A class representing and parsing pool URIs.
  *
- * Created: Thu Jul  1 15:56:36 2010
+ * <p> The location of a pool consists of two parts: the address of
+ * its server and the pool name. One can specify the former as a
+ * {@link PoolServerAddress} object, accompanying it by the pool name
+ * as a string to create the full pool address.
+ *
+ * <p> As an alternative, one can also use a single string with the
+ * full pool URI and rely on PoolAddress to handle its parsing (or on
+ * any of the methods in {@link Pool} that take a single string as
+ * their pool locator).
+ *
+ * <p> As an example, this code snippet:
+ * <pre>
+ *   PoolServerAddress = new PoolServerAddress("tcp", "hostname", 2323);
+ *   PoolAddress a = new PoolAddress(sa, "a-pool");
+ * </pre>
+ *
+ * creates a PoolAddress that is the same as the one created as:
+ * <pre>
+ *   PoolAddress a = PoolAddress.fromURI("tcp://hostname:2323/a-pool");
+ * </pre>
  *
  * @author jao
  */
 @Immutable
 public final class PoolAddress {
 
+    /** Constructs a PoolAddress object from its string representation. */
     public static PoolAddress fromURI(String uri)
-        throws PoolException {
+        throws BadAddressException {
         return new PoolAddress(null, uri);
     }
 
+    /**
+     * Combines a server address and a pool name to create a PoolAddress.
+     *
+     * <p> {@code name} will usually be a path relative to the server
+     * address (e.g. {@code "pname"} or {@code "rel/path/name of a
+     * pool"}), and this constructor will combine the two to yield the
+     * final pool address.
+     *
+     * <p> But, if {@code name} happens to be a fully qualified pool
+     * URI (e.g. {@code "tcp://there/a-pool"}, {@code addr} will be
+     * ignored, and the new PoolAddress will be equal to one created
+     * using {@code fromURI(name)}.
+     *
+     * <p> If {@code addr} is null, a default server address (as
+     * constructed by {@code new PoolServerAddress(null)}, i.e.,
+     * {@code "tcp://localhost"} will be used.
+     *
+     * @throws BadAddressException if {@code name} is not a valid pool
+     * name or URI. Note, however, that the lack of an error when
+     * constructing an address on the client side doesn't guarantee
+     * that the server will accept the pool name as a valid one.
+     */
     public PoolAddress(PoolServerAddress addr, String name)
-        throws PoolException {
+        throws BadAddressException {
         Matcher matcher = ADDR_PATT.matcher(name);
         if (!PoolServerAddress.isRelative(name)
             && matcher.matches() && matcher.group(1) != null) {
@@ -36,9 +79,18 @@ public final class PoolAddress {
         stringRep = serverAddress + "/" + poolName;
     }
 
+    /** Accessor to the address of the server this pools belongs to. */
     public PoolServerAddress serverAddress() { return serverAddress; }
+    /** Accesor to the part of the address relative to the server address. */
     public String poolName() { return poolName; }
 
+    /**
+     * Returns a valid URI representing this address.
+     * <p> That means that it is always the case that:
+     * <pre>
+     *   address == PoolAddress.fromURI(address.toString());
+     * </pre>
+     */
     @Override public String toString() {
         return stringRep;
     }
@@ -58,7 +110,8 @@ public final class PoolAddress {
 
     private static final Pattern ADDR_PATT = Pattern.compile(ADDR_REGEX);
 
-    private static final String checkName(String name) throws PoolException {
+    private static final String checkName(String name)
+        throws BadAddressException {
         if (name == null || name.isEmpty())
             throw new BadAddressException("Empty pool name");
         // we leave any further checking to the pool server
--- src/com/oblong/jelly/PoolServerAddress.java
+++ src/com/oblong/jelly/PoolServerAddress.java
@@ -1,33 +1,51 @@
 // Copyright (c) 2010 Oblong Industries
+// Created: Wed Jun  9 16:20:42 2010
 
 package com.oblong.jelly;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import net.jcip.annotations.Immutable;
 
 /**
+ * A wrapper around URIs representing pool server addresses.
  *
- * Created: Wed Jun  9 16:20:42 2010
+ * <p> To connect to a pool server we need to know its host, a port
+ * and the protocol or scheme it uses. All these three parameters can
+ * be specified in one shot as a string, but sometimes it's handy to
+ * use a more structured data. PoolServerAddress offers the latter.
+ *
+ * <p> Most of the time, the scheme for the pool servers you use will
+ * be {@code "tcp"}, so you may use most of this class API taking that
+ * as a default.
  *
  * @author jao
  */
 @Immutable
 public final class PoolServerAddress {
 
+    /** Default connection port for server addresses ({@value}). */
     public static final int DEFAULT_PORT = 65456;
+
+    /** Default scheme for server addresses ({@value}). */
     public static final String DEFAULT_SCHEME = "tcp";
+
+    /** Default host for server addresses ({@value}). */
     public static final String DEFAULT_HOST = "localhost";
 
-    public static boolean isRelative(String uri) {
-        Matcher matcher = ADDR_PATT.matcher(uri);
-        return (!matcher.lookingAt()
-                || matcher.group(1) == null
-                || matcher.group(1).isEmpty());
-    }
-
+    /**
+     * Creates a pool address by parsing the given string.
+     *
+     * <p> If any of the three components in a server address (scheme,
+     * host, port) is missing, default values are provided.
+     *
+     * <p> The expected format and parsing rules are, for the most
+     * part, those used for the server part of URLs.
+     *
+     * @throws BadAddressException {@code uri} is not a valid pool
+     * name or URI.
+     */
     public static PoolServerAddress fromURI(String uri)
         throws BadAddressException {
         Matcher matcher = ADDR_PATT.matcher(uri);
@@ -39,6 +57,20 @@ public final class PoolServerAddress {
         return new PoolServerAddress(scheme, host, p);
     }
 
+    /**
+     * Constructs a new server address out of its three components.
+     * Any of its first two arguments can be null, in which case the
+     * default values given by {@code DEFAULT_SCHEME} and {@code
+     * DEFAULT_HOST} are used. If {@code port} is negative, {@code
+     * DEFAULT_PORT} is used instead.
+     *
+     * <p> {@code host} can be either a human readable hostname or a
+     * string representation of an IP address (for schemes, such as
+     * TCP, using that addressing protocol).
+     *
+     * @throws BadAddressException {@code uri} is {@code host} is not
+     * a valid host name.
+     */
     public PoolServerAddress(String scheme, String host, int port)
         throws BadAddressException {
          this.scheme = checkScheme(scheme);
@@ -49,10 +81,19 @@ public final class PoolServerAddress {
              + (this.port == DEFAULT_PORT ? "" : ":" + this.port);
     }
 
-    public PoolServerAddress(String host, int port) throws BadAddressException {
+    /**
+     * Equivalent to {@code PoolServerAddress(DEFAULT_SCHEME, host,
+     * port)}.
+     */
+    public PoolServerAddress(String host, int port)
+        throws BadAddressException {
         this("", host, port);
     }
 
+    /**
+     * Equivalent to {@code PoolServerAddress(DEFAULT_SCHEME, host,
+     * DEFAULT_PORT)}.
+     */
     public PoolServerAddress(String host) throws BadAddressException {
         this("", host, -1);
     }
@@ -73,8 +114,27 @@ public final class PoolServerAddress {
         return toString().hashCode();
     }
 
+    /**
+     * Returns a valid URI representing this address.
+     * <p> That means that it is always the case that:
+     * <pre>
+     *   address == PoolServerAddress.fromURI(address.toString());
+     * </pre>
+     */
     @Override public String toString() {
         return stringRep;
+    }
+
+    /**
+     * Auxliarily function to help determine whether a URI is contains
+     * a pool address part or should be considered just a pool name.
+     * Client code probably won't use this method at all.
+     */
+    public static boolean isRelative(String uri) {
+        Matcher matcher = ADDR_PATT.matcher(uri);
+        return (!matcher.lookingAt()
+                || matcher.group(1) == null
+                || matcher.group(1).isEmpty());
     }
 
     static final String ADDR_REGEX =
