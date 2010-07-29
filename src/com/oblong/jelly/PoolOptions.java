96e2bffd4b9be638a64f75898e2826f08410d656
jjj <jjj@oooooo.com>
2010-07-29 02:04:42 +0200

    Docstrings for PoolOptions.

--- src/com/oblong/jelly/PoolOptions.java
+++ src/com/oblong/jelly/PoolOptions.java
@@ -2,40 +2,87 @@
 
 package com.oblong.jelly;
 
+import net.jcip.annotations.Immutable;
+
 /**
+ * Options for newly created pools.
+ *
+ * This class lets you specify creation options for new pools. It also
+ * defines several pre-cooked instances with typical parameter values.
  *
  * Created: Thu Jun 10 20:56:49 2010
  *
  * @author jao
  */
+@Immutable
 public class PoolOptions {
 
+    /**
+     * Options specifying a pool of 1Mb with an index with a capacity
+     * of 1024 entries.
+     */
     public static final PoolOptions SMALL;
+
+    /**
+     * Options specifying a pool of 10Mb with an index with a capacity
+     * of 10K entries.
+     */
     public static final PoolOptions MEDIUM;
+
+    /**
+     * Options specifying a pool of 100Mb with an index with a capacity
+     * of 100K entries.
+     */
     public static final PoolOptions LARGE;
+
+    /**
+     * Options specifying a pool of 1Gb with an index with a capacity
+     * of one million entries.
+     */
     public static final PoolOptions HUGE;
 
+    /**
+     * For Pool implementers defining a new pool type or server
+     * protocol.
+     */
     public PoolOptions(Slaw options) {
         this.options = options == null ? SMALL.options : options;
     }
 
-    public PoolOptions(int size) {
-        options = Slaw.map(SIZE_KEY, Slaw.int32(size));
+    /**
+     * Creates an options instance specifying a pool size of the given
+     * number of bytes. For mmap pools, this size is the total disk
+     * space occupied by the file in the server backing the pool.
+     */
+    public PoolOptions(long size) {
+        options = Slaw.map(SIZE_KEY, Slaw.unt64(size));
     }
 
-    public PoolOptions(long size, long indexSize) {
+    /**
+     * Creates an options instance specifying a pool size of the given
+     * number of bytes and an index with the given number of entries.
+     * Pools of type mmap use an index to speed up lookups; for
+     * optimal performance, its capacity should be close to the
+     * (average) expected number of proteins in the pool. Note that
+     * every entry on the index contributes 16 bytes to its size.
+     */
+    public PoolOptions(long size, long indexCapacity) {
         options = Slaw.map(SIZE_KEY, Slaw.unt64(size),
-                           INDEX_SIZE_KEY, Slaw.unt64(indexSize));
+                           INDEX_SIZE_KEY, Slaw.unt64(indexCapacity));
     }
 
     public long poolSize() {
         return getLong(SIZE_KEY);
     }
 
-    public long indexSize() {
+    public long indexCapacity() {
         return getLong(INDEX_SIZE_KEY);
     }
 
+    /**
+     * Options are transmitted through the wire as a Protein. Chances
+     * are you'll get little use of this method.
+     */
     public Protein toProtein() {
         return Slaw.protein(null, options);
     }
