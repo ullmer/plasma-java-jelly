2c71c8304ba9ab12aea0ed39cbc1ae917a51a355
jjj <jjj@oooooo.com>
2010-09-01 22:47:04 +0200

    Android shared lib and application skeleton

---
+++ droid/ponder/src/com/oblong/android/ponder/PonderActivity.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2010 Oblong Industries
+
+package com.oblong.android.ponder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.oblong.jelly.Slaw;
+
+public class PonderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
