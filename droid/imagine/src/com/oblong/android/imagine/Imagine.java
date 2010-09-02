8f7eeab48c42cce55454f73aa0f437b743c2edce
jjj <jjj@oooooo.com>
2010-09-02 20:31:07 +0200

    New droid: imagine

---
+++ droid/imagine/src/com/oblong/android/imagine/Imagine.java
@@ -0,0 +1,16 @@
+package com.oblong.android.imagine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Imagine extends Activity {
+    /** Called when the activity is first created. */
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        // cameraView = new CameraView(this);
+        // setContentView(cameraView);
+    }
+    
+    // CameraView cameraView;
+}
