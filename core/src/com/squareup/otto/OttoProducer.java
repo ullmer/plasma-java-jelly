db802bc4c744999c88c2c81178068d2db5a85c3e
jjj <jjj@oooooo.com>
2013-12-05 13:07:48 +0100

    Made Otto Bus not dependent on Android and moved to jelly (could be a separate module on top of jelly actually)

---
+++ core/src/com/squareup/otto/OttoProducer.java
@@ -0,0 +1,11 @@
+package com.squareup.otto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karol
+ * Date: 8/1/13
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OttoProducer extends OttoRegistree {
+}
---
+++ core/src/com/squareup/otto/OttoRegistree.java
@@ -0,0 +1,11 @@
+package com.squareup.otto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karol
+ * Date: 8/1/13
+ * Time: 5:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OttoRegistree {
+}
