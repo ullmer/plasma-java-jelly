286afe5a4ae52b4edff212062a75cbe77ee44dea
jjj <jjj@oooooo.com>
2014-01-25 21:56:18 +0100

    Changed class sig: MapField<T extends UnmarshalledSlawMap>

--- core/src/com/oblong/jelly/schema/fields/MapField.java
+++ core/src/com/oblong/jelly/schema/fields/MapField.java
@@ -4,6 +4,8 @@ import com.oblong.jelly.Slaw;
 import com.oblong.jelly.schema.SlawSchema;
 import com.oblong.jelly.slaw.java.SlawMap;
 
+import java.util.Map;
+
 /**
  *
  * TODO: consider making it "extends AbstractField<T>", for consistency with ListField
@@ -15,7 +17,7 @@ import com.oblong.jelly.slaw.java.SlawMap;
  * Date: 7/10/13
  * Time: 1:38 PM
  */
-public class MapField extends AbstractField<SlawMap> {
+public class MapField<T extends UnmarshalledSlawMap> extends AbstractField<T> {
 
 	public MapField(SlawSchema schema, boolean isOptional, String name, AbstractField[] fields) {
 		super(schema, isOptional, name);
@@ -26,12 +28,19 @@ public class MapField extends AbstractField<SlawMap> {
 	}
 
 	@Override
-	protected SlawMap getCustom(Slaw slaw) {
-		return (SlawMap) slaw;
+	protected T getCustom(Slaw slaw) {
+		throw new UnsupportedOperationException("This is handled in ProteinLang getters generation, instead of here," +
+				"to avoid using reflection or having a need for bloated number of inner classes.");
 	}
 
 	@Override
-	public Slaw toSlaw(SlawMap value) {
-		return value;
+	public SlawMap getRawSlawFrom(Map<Slaw, Slaw> map) {
+		return (SlawMap) super.getRawSlawFrom(map);
 	}
+
+	@Override
+	public SlawMap toSlaw(T value) {
+		return value.toSlaw();
+	}
+
 }
