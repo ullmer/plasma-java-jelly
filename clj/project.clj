64703c5c0baf59a77abd2980b40ba0ca3790f304
jjj <jjj@oooooo.com>
2014-04-02 18:47:19 +0200

    Added new local maven repo to clojure dir

--- clj/project.clj
+++ clj/project.clj
@@ -1,21 +1,15 @@
 (defproject jelly "0.1.5-SNAPSHOT"
   :description "jelly's Clojure bindings and REPL"
-
+  :repositories {"local" "file:repo"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [org.yaml/snakeyaml "1.7"]
-                 [net.jcip/jcip-annotations "1.0"]
-                 [log4j/log4j "1.2.14"]
-		 [ob-tls-helpers "1.0"]]
+		 [jelly-standalone "0.1"]]
 
- 
-  :repositories {"ob-tls-helpers" ~(str(.toURI(java.io.File. "maven_repository")))}
   :dev-dependencies [[swank-clojure "1.3.4"]
                      [lein-marginalia "0.7.0"]]
 
   :disable-implicit-clean false
 
-  :java-source-path "../core/src"
   :java-options {:debug "true" :fork "true" :target "1.6"}
 
   :main jelly.main)
