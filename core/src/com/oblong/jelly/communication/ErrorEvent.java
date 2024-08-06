0deda80f35c3828c696fe84340b6f5ed91d4b65c
Brygg Ullmer <ullmer@acm.org>
2024-08-06 11:30:37 -0400

    otto is deprecated/2019; commenting out, will see where issues arise

--- core/src/com/oblong/jelly/communication/ErrorEvent.java
+++ core/src/com/oblong/jelly/communication/ErrorEvent.java
@@ -1,11 +1,11 @@
 package com.oblong.jelly.communication;
 
-import com.squareup.otto.*;
+//import com.squareup.otto.*;
 
 /**
  * User: karol
  * Date: 12/3/13
  * Time: 6:26 PM
  */
-public class ErrorEvent implements BusEvent {
+public class ErrorEvent { //implements BusEvent {
 }
