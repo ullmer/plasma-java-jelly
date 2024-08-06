26d6880a94a5d237f2024297a0d94620b6eced00
Brygg Ullmer <ullmer@acm.org>
2024-08-06 15:05:11 -0400

    restoring some otto codebases -- even though deprecated, may try to bootstrap with it

---
+++ core/src/com/squareup/otto/OttoProducer.java
@@ -0,0 +1,11 @@
+package com.squareup.otto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karol
+ * Date: 8/1/13
+ * Time: 5:56 PM
+ *
+ */
+public interface OttoProducer extends OttoRegistree {
+}
---
+++ core/src/com/squareup/otto/OttoSubscriber.java
@@ -0,0 +1,11 @@
+package com.squareup.otto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karol
+ * Date: 8/1/13
+ * Time: 5:44 PM
+ *
+ */
+public interface OttoSubscriber extends OttoRegistree {
+}
