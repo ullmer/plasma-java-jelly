11f84809b25f9ada97873cbee6c904786defd5f5
Brygg Ullmer <ullmer@acm.org>
2024-08-06 23:38:02 -0400

    c replication for evolution

---
+++ core/test/com/oblong/jelly/p-await-helloWorld02.java
@@ -0,0 +1,86 @@
+
+/* (c)  oblong industries */
+
+///
+/// Loop forever reading proteins from a pool with pool_await_next().
+///
+#include "pool_cmd.h"
+#include "libLoam/c/ob-sys.h"
+#include "libLoam/c/ob-vers.h"
+#include "libLoam/c/ob-log.h"
+#include "libPlasma/c/slaw.h"
+#include "libPlasma/c/slaw-io.h"
+#include "libPlasma/c/protein.h"
+
+static void usage (void)
+{
+  ob_banner (stderr);
+  fprintf (stderr, "Usage: p-await <pool name>\n");
+  exit (EXIT_FAILURE);
+}
+
+int main (int argc, char **argv)
+{
+  OB_CHECK_ABI ();
+
+  ob_retort pret;
+  protein p;
+  pool_timestamp ts;
+  pool_cmd_info cmd;
+
+  memset(&cmd, 0, sizeof(cmd));
+
+  const char *pnstr = "tcp://localhost/hello";
+  cmd.pool_name     = pnstr;
+
+  //if (pool_cmd_get_poolname (&cmd, argc, argv, optind))
+  //  usage ();
+
+  pool_cmd_open_pool (&cmd);
+
+  while (1)
+    {
+      pret = pool_await_next (cmd.ph, POOL_WAIT_FOREVER, &p, &ts, NULL);
+      if (OB_OK != pret)
+        {
+          pool_withdraw (cmd.ph);
+          fprintf (stderr, "problem with pool_await_next(): %s\n",
+                   ob_error_string (pret));
+          return pool_cmd_retort_to_exit_code (pret);
+        }
+      bslaw d = protein_descrips(p);
+      bslaw i = protein_ingests(p);
+
+      //slaw *sl = malloc(1000);
+      //slaw_to_string(d, sl);
+      //char *st = slaw_string_emit(d);
+
+      char *str1 = slaw_list_emit_first(d);
+      printf("STR1: %s\n", str1); 
+
+      void *map = slaw_list_emit_first(i);
+
+
+      //char *str2 = slaw_cons_emit_cdr(i);
+      //char *str3 = slaw_cons_emit_car(i);
+
+      char *str2 = slaw_cons_emit_car(map);
+      char *str3 = slaw_cons_emit_cdr(map);
+
+      printf("STR2: %s\n", str2); 
+      printf("STR3: %s\n", str3); 
+      
+      fflush(stdout);
+      //slaw_spew_overview (p, stdout, NULL);
+      printf("I3\n");
+      slaw_spew_overview (i, stdout, NULL);
+      fputc ('\n', stdout);
+      protein_free (p);
+    }
+
+  // Not reached at present.
+  OB_DIE_ON_ERROR (pool_withdraw (cmd.ph));
+  pool_cmd_free_options (&cmd);
+
+  return EXIT_SUCCESS;
+}
---
+++ core/test/com/oblong/jelly/p-deposit-helloWorld2.java
@@ -0,0 +1,100 @@
+/* (c)  oblong industries */
+
+// Brygg Ullmer: reduction to approximation of JShrake helloWorld example from Animist discord #plasma 
+
+///
+/// Deposit a protein into a pool.
+///
+#include "pool_cmd.h"
+#include "libLoam/c/ob-log.h"
+#include "libLoam/c/ob-sys.h"
+#include "libLoam/c/ob-vers.h"
+#include "libPlasma/c/protein.h"
+#include "libPlasma/c/slaw.h"
+
+static slaw extract_slaw (char *arg)
+{
+  char *colon = strchr (arg, ':');
+  slaw key, value, pair;
+
+  if (colon == NULL)
+    { fprintf (stderr, "error: ingest '%s' needs a colon to separate key and value\n", arg);
+      exit (EXIT_FAILURE);
+    }
+
+  char *keystr = (char *) malloc (colon - arg + 1);
+  strncpy (keystr, arg, colon - arg);
+  keystr[colon - arg] = '\0';
+  key = slaw_string (keystr);
+  free (keystr);
+
+  do
+    { char *endptr;
+      int64 int_val = strtol (colon + 1, &endptr, 10);
+      if (*endptr == '\0')
+        { value = slaw_int64 (int_val);
+          break;
+        }
+      float64 float_val = strtod (colon + 1, &endptr);
+      if (*endptr == '\0')
+        { value = slaw_float64 (float_val);
+          break;
+        }
+      value = slaw_string (colon + 1);
+    }
+  while (0);
+
+  pair = slaw_cons_ff (key, value);
+  return pair;
+}
+
+int main (int argc, char **argv)
+{ OB_CHECK_ABI ();
+
+  ob_retort     pret;
+  pool_cmd_info cmd;
+  protein       prot;
+  slaw          ingest;
+  int           c;
+
+  //jshrake hello-world example
+  //https://media.discordapp.net/attachments/1235355937586413578/1252001896156102736/image.png?ex=669196a1&is=66904521&hm=fb7b16cd19a85a6723e48956e91f3a262007e2b5db71b99be5a96a191585787c
+
+  const char *dstr  = "hello";
+  const char *istr  = "name:world";
+  const char *pnstr = "tcp://localhost/hello";
+
+  memset(&cmd, 0, sizeof(cmd));
+
+  slabu *descrips = slabu_new ();
+  slabu *ingests  = slabu_new ();
+
+  ingest = extract_slaw (istr);
+  OB_DIE_ON_ERROR (slabu_list_add_c (descrips, dstr));
+  OB_DIE_ON_ERROR (slabu_list_add_x (ingests, ingest));
+
+  cmd.verbose   = 1;
+  cmd.pool_name = pnstr;
+
+  pool_cmd_open_pool (&cmd);
+
+  prot = protein_from_ff (slaw_list_f (descrips), slaw_map_f (ingests));
+  if (cmd.verbose)
+    { fprintf (stderr, "depositing in %s\n", cmd.pool_name);
+      slaw_spew_overview (prot, stderr, NULL);
+    }
+
+  pret = pool_deposit (cmd.ph, prot, NULL);
+  protein_free (prot);
+
+  if (OB_OK != pret)
+    { fprintf (stderr, "no luck on the deposit: %s\n", ob_error_string (pret));
+      exit (pool_cmd_retort_to_exit_code (pret));
+    }
+
+  OB_DIE_ON_ERROR (pool_withdraw (cmd.ph));
+
+  return EXIT_SUCCESS;
+}
+
+/// end ///
