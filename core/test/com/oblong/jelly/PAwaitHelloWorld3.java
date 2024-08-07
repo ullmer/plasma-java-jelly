cacbdf6da3960cc95860ddd6fc64e5f7eb48a067
Brygg Ullmer <ullmer@acm.org>
2024-08-07 17:59:44 -0400

    duplicating PAwaitHelloWorld2.java; will tweak to make async

---
+++ core/test/com/oblong/jelly/PAwaitHelloWorld3.java
@@ -0,0 +1,76 @@
+// Brygg Ullmer: reduction to approximation of JShrake helloWorld example from Animist discord #plasma 
+/// Synchronously await a protein from a pool (with blocking awaitNext call).  
+///  Sister variant PAwaitHelloWorld3 will hopefully demonstrate async version.
+// Begun August 7, 2024
+
+import java.util.Map; 
+import java.util.HashMap; 
+
+import com.oblong.jelly.Slaw;
+import com.oblong.jelly.Protein;
+import com.oblong.jelly.Hose;
+import com.oblong.jelly.Pool;
+import com.oblong.jelly.PoolException;
+
+import static com.oblong.jelly.Slaw.*;
+import static com.oblong.jelly.Protein.*;
+
+public class PAwaitHelloWorld2 {
+
+   static String pnstr = "tcp://localhost/hello";
+
+   public PAwaitHelloWorld2() {System.out.println("pahw2 constructor");}
+
+   public static void main(String[] args) {
+     System.out.println("p-await hello, world 2");
+     Hose h = null;
+
+     try {
+       h = Pool.participate(pnstr);
+       while (true) {
+         Protein p = h.awaitNext();
+
+	 Slaw d = p.descrips();
+         Slaw i = p.ingests();
+
+	 if (d.isList()) {
+           //String str1 = d.nth(0).emitString();
+           String str1 = d.car().emitString();
+	   System.out.println("STR1:" + str1);
+	 } else {
+           System.err.println("Message received; descrips is not a list");
+	 }
+
+	 if (i.isMap()) {
+           Map<Slaw,Slaw> imap = i.emitMap();
+
+	   for (Map.Entry<Slaw,Slaw> entry : imap.entrySet()) {
+              Slaw key   = entry.getKey();
+	      Slaw value = entry.getValue();
+
+	      if (key.isString() && value.isString()) {
+                String str2 = key.emitString();
+                String str3 = value.emitString();
+	
+	        System.out.println("STR2:" + str2);
+    	        System.out.println("STR2:" + str3);
+	     } else {
+               System.err.println("Message received; ingests is a map, but entry does not map strings to strings");
+	     }
+	   }
+	 } else {
+           System.err.println("Message received; ingests is not a map");
+	 }
+       }
+     } catch (PoolException e) {
+       System.out.println("Pool exception:" + e);
+       System.exit(-1);
+
+     } finally {
+       if (h != null) h.withdraw();
+    }
+    System.out.println("p-await hello, world 3 ends");
+  } 
+}
+
+/// end ///
