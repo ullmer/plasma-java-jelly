725d38a7b9dd2463f5efa00c96d87477e932cc27
Brygg Ullmer <ullmer@acm.org>
2024-09-20 12:31:07 -0400

    adding thread checker engagement logic

--- core/src/com/oblong/jelly/pool/mem/MemHose.java
+++ core/src/com/oblong/jelly/pool/mem/MemHose.java
@@ -18,6 +18,10 @@ public class MemHose implements Hose {
 	private static final long NO_LAST_PROTEIN = -999;
 	protected final ThreadChecker threadChecker = new ThreadChecker();
 
+        public bool isThreadCheckerEngaged() {return threadChecker.isThreadCheckerEngaged();} // BAU
+        public void engageThreadChecker()    {threadChecker.engageThreadChecker();}           // BAU
+        public void disengageThreadChecker() {threadChecker.disengageThreadChecker();}        // BAU
+
 	/** NOTE: might not be zero after connecting - we might need to initialize it to proper value upon joining the
 	 * pool */
     private long lastObtainedProteinIndex = NO_LAST_PROTEIN;
